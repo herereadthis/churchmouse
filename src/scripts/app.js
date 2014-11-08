be6fe3f3af720f67b95a10129dbf6f91ba4ad482
herereadthis <herereadthis@gmail.com>
2014-11-07 23:05:49 -0500

    adds funtime view for testing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,25 +9,29 @@
  * Main module of the application.
  */
 angular
-  .module('churchMouseApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('churchMouseApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl'
+        })
+        .when('/funtime', {
+            templateUrl: 'views/funtime.html',
+            controller: 'FunTimeCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    });
---
+++ src/scripts/controllers/funtime.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name churchMouseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the churchMouseApp
+ */
+angular.module('churchMouseApp')
+    .controller('FunTimeCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
