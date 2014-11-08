11b15a9d9f48550def550e9348f383e7b3d7b469
herereadthis <herereadthis@gmail.com>
2014-11-07 23:30:27 -0500

    adds masters json to templates

--- src/scripts/controllers/funtime.js
+++ src/scripts/controllers/funtime.js
@@ -2,13 +2,17 @@
 
 /**
  * @ngdoc function
- * @name churchMouseApp.controller:MainCtrl
+ * @name churchMouseApp.controller:FunTimeCtrl
  * @description
- * # MainCtrl
+ * # FunTimeCtrl
  * Controller of the churchMouseApp
  */
+
 angular.module('churchMouseApp')
-    .controller('FunTimeCtrl', function ($scope) {
+    .controller('FunTimeCtrl', function ($scope, $http) {
+        $http.get('/json/masters.json').success(function(data) {
+            $scope.masters = data;
+        });
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
