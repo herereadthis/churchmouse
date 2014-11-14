30cd4689d3153f4ce46a32e4df3a3b3389bf7b0b
herereadthis <herereadthis@gmail.com>
2014-11-13 22:10:40 -0500

    functioning tab selector

--- src/scripts/components/tabs/tabs.js
+++ src/scripts/components/tabs/tabs.js
@@ -2,6 +2,63 @@
 
 
 angular.module('churchMouseApp')
+    .controller('TabsController', ['$scope', function ($scope) {
+        $scope.foobar = {
+            foo: 'foo',
+            bar: 'bar'
+        };
+    }])
+    .directive('myTabs', function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+            controller: function($scope) {
+                var panes = $scope.panes = [];
+
+                $scope.select = function(pane) {
+                    angular.forEach(panes, function(pane) {
+                        pane.selected = false;
+                    });
+                    pane.selected = true;
+                };
+
+                this.addPane = function(pane) {
+                    if (panes.length === 0) {
+                        $scope.select(pane);
+                    }
+                    panes.push(pane);
+                };
+            },
+            templateUrl: 'scripts/components/tabs/my-tabs.html'
+        };
+    })
+    .directive('myPane', function() {
+        return {
+            require: '^myTabs',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                title: '@'
+            },
+            link: function(scope, element, attrs, TabsController) {
+                TabsController.addPane(scope);
+            },
+            templateUrl: 'scripts/components/tabs/my-pane.html'
+        }
+    });
+
+
+
+
+
+
+
+/*
+'use strict';
+
+
+angular.module('churchMouseApp')
     .controller('TabsController', function() {
     })
     .directive('myTabs', function() {
@@ -11,4 +68,5 @@ angular.module('churchMouseApp')
             templateUrl: 'scripts/components/tabs/my-tabs.html'
         }
     });
+*/
 
