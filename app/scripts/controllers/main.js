3e7ade551c8b8398b6b93a83dac4521c14fbb3d6
herereadthis <herereadthis@gmail.com>
2015-04-12 21:10:40 -0400

    #18 image cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,27 +18,27 @@ angular.module('churchMouseApp')
         $scope.imageCarousel = [
             {
                 id: 0,
-                src: '/images/dsc4090_1800x1200.jpg',
+                src: '/images/carousel/dsc4090_1800x1200.jpg',
                 description: 'image'
             },
             {
                 id: 1,
-                src: '/images/placeholder_1008x567.jpg',
+                src: '/images/carousel/placeholder_1008x567.jpg',
                 description: 'image'
             },
             {
                 id: 2,
-                src: '/images/va1_432x288.jpg',
+                src: '/images/carousel/va1_432x288.jpg',
                 description: 'image'
             },
             {
                 id: 3,
-                src: '/images/placeholder_1600x1280.jpg',
+                src: '/images/carousel/placeholder_1600x1280.jpg',
                 description: 'image'
             },
             {
                 id: 4,
-                src: '/images/dsc5138_1800x1200.jpg',
+                src: '/images/carousel/dsc5138_1800x1200.jpg',
                 description: 'image'
             }
         ];
